30f9c8925d0e1c791496071ded3d138a7ae66f73
Mei <47629641+MeiNogizaka@users.noreply.github.com>
2019-03-29 14:59:19 +0900

    Update cisco_nxos_ssh.py

--- netmiko/cisco/cisco_nxos_ssh.py
+++ netmiko/cisco/cisco_nxos_ssh.py
@@ -30,7 +30,29 @@ class CiscoNxosSSH(CiscoSSHConnection):
             check_string=check_string, pattern=pattern
         )
 
+class CiscoNxosTelnet(CiscoSSHConnection):
+    def session_preparation(self):
+        """Prepare the session after the connection has been established."""
+        self._test_channel_read(pattern=r"[>#]")
+        self.ansi_escape_codes = True
+        self.set_base_prompt()
+        self.disable_paging()
+        # Clear the read buffer
+        time.sleep(0.3 * self.global_delay_factor)
+        self.clear_buffer()
 
+    def normalize_linefeeds(self, a_string):
+        """Convert '\r\n' or '\r\r\n' to '\n, and remove extra '\r's in the text."""
+        newline = re.compile(r"(\r\r\n|\r\n)")
+        # NX-OS fix for incorrect MD5 on 9K (due to strange <enter> patterns on NX-OS)
+        return newline.sub(self.RESPONSE_RETURN, a_string).replace("\r", "\n")
+
+    def check_config_mode(self, check_string=")#", pattern="#"):
+        """Checks if the device is in configuration mode or not."""
+        return super(CiscoNxosTelnet, self).check_config_mode(
+            check_string=check_string, pattern=pattern
+        )
+    
 class CiscoNxosFileTransfer(CiscoFileTransfer):
     """Cisco NXOS SCP File Transfer driver."""
 
