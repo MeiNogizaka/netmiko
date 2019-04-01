43859b8ca0689d053966b4def4f56f02dc00c844
Mei <47629641+MeiNogizaka@users.noreply.github.com>
2019-04-01 19:09:57 +0900

    Update __init__.py

--- netmiko/cisco/__init__.py
+++ netmiko/cisco/__init__.py
@@ -9,7 +9,7 @@ from netmiko.cisco.cisco_ios import CiscoIosFileTransfer
 from netmiko.cisco.cisco_ios import InLineTransfer
 from netmiko.cisco.cisco_asa_ssh import CiscoAsaSSH, CiscoAsaFileTransfer
 from netmiko.cisco.cisco_nxos_ssh import CiscoNxosSSH, CiscoNxosTelnet, CiscoNxosFileTransfer
-from netmiko.cisco.cisco_xr_ssh import CiscoXrSSH, CiscoXrFileTransfer
+from netmiko.cisco.cisco_xr_ssh import CiscoXrSSH, CiscoXrTelnet, CiscoXrFileTransfer
 from netmiko.cisco.cisco_wlc_ssh import CiscoWlcSSH
 from netmiko.cisco.cisco_s300 import CiscoS300SSH
 from netmiko.cisco.cisco_tp_tcce import CiscoTpTcCeSSH
@@ -21,6 +21,7 @@ __all__ = [
     "CiscoNxosSSH",
     "CiscoNxosTelnet",
     "CiscoXrSSH",
+    "CiscoXrTelnet",
     "CiscoWlcSSH",
     "CiscoS300SSH",
     "CiscoTpTcCeSSH",
